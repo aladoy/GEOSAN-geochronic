b7c0d811bbbcb4a2794a7550d452f3bf98aa70fa
aladoy <anais.ladoy@epfl.ch>
2023-02-13 11:12:32 +0100

    assign RELI to F2 participants

--- src/wrangling/assign_to_reli.py
+++ src/wrangling/assign_to_reli.py
@@ -1,8 +1,5 @@
 '''This code assign Follow-up participants to the RELI (inhabited hectare) where they live.'''
 
-
-import numpy as np
-from shapely.ops import nearest_points
 import pandas as pd
 import geopandas as gpd
 import sys
@@ -12,52 +9,49 @@ import os
 sys.path.append(r"/mnt/data/GEOSAN/FUNCTIONS/GIRAPH-functions/")
 try:
     import db_utils as db
-    import basic_utils as bu
+    import basic_utils as bu  # commit ede2be490aa3b2cabeb82bd514b9376c80ea573a
 except FileNotFoundError:
     print("Wrong file or file path")
 
 
-project_dir: str = r"/mnt/data/GEOSAN/RESEARCH PROJECTS/COLAUS NCDS @ LASIG (EPFL)/GEOSAN-colaus-ncds/"
+def main():
 
-engine, conn, cursor = db.connect_db("geosan", "aladoy")
+    engine, conn, cursor = db.connect_db("geosan", "aladoy")
 
-# Extract F2 participants and inhabited hectares (2021)
-indiv = gpd.read_postgis("SELECT * FROM syndemic.colaus_f2_complete",
-                         conn, geom_col="geometry")
-reli = gpd.read_postgis("SELECT * FROM vd_reli_polygon",
-                        conn, geom_col="geometry")
+    # Extract F2 participants and inhabited hectares (2021)
+    indiv = gpd.read_postgis("SELECT * FROM geochronic.f2_study_dataset",
+                             conn, geom_col="geometry")
+    reli = gpd.read_postgis("SELECT * FROM vd_reli_polygon",
+                            conn, geom_col="geometry")
+
+    # Restrict searching radius to 300 meters
+    indiv['reli'] = indiv.apply(bu.find_nearest_reli, args=(reli, 300), axis=1)
+
+    print("Do all individuals have a RELI (check from DataFrame)? " +
+          str(indiv[indiv.reli.isna()].shape[0] == 0))
+    print("If some individuals have not be assigned to a RELI (searching radius too small), the UPDATE query will throw an error because the attribute RELI will be float and not integer.")
+
+    # Add this to GEOSAN DB table
+    table_name = "geochronic.f2_study_dataset"
+    cursor.execute(f"ALTER TABLE {table_name} DROP COLUMN reli;")
+    conn.commit()
+    cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN reli integer;")
+    conn.commit()
+
+    for i, value in enumerate(indiv["reli"]):
+
+        pt = indiv.loc[i, "pt"]
+        cursor.execute(
+            f"UPDATE {table_name} SET reli = '{value}' WHERE pt = {pt}")
+
+    conn.commit()
+
+    cursor.execute(f"SELECT * FROM {table_name} WHERE reli IS NULL")
+    print("Check that everything was correctly inserted into DB " +
+          str(len(cursor.fetchall()) == 0))
+
+    conn.close()
 
 
-indiv['reli'] = indiv.apply(find_nearest_reli, args=(reli, 500), axis=1)
-
-indiv[indiv.reli.isna()]
-
-def find_nearest_reli(row, ha_poly, radius=None):
-    """
-    This function retrieves the nearest neighbor (Point) from a given Point.
-    To speed-up the process, the user can restrict the set of destination
-    points to a given neighborhood (radius around the point of origin).
-    Outputs: nearest RELI index (NaN for isolated point)
-    """
-    try:
-        nearest_reli = ha_poly[ha_poly.geometry.contains(
-            row.geometry)]['reli'].values[0]
-
-    except IndexError:
-
-        try:
-
-            if radius is not None:
-
-                ha_poly = ha_poly.loc[
-                    ha_poly.intersects(row.geometry.buffer(radius))
-                ]
-
-            ha_poly['distance'] = ha_poly.geometry.distance(row.geometry)
-            ha_poly = ha_poly.sort_values(by='distance')
-            nearest_reli = ha_poly.iloc[0]['reli']
-
-        except IndexError:
-            nearest_reli = np.nan  # no hectares within this distance
-
-    return nearest_reli
+if __name__ == "__main__":
+    main()
